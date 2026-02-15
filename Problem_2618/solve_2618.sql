9c23cbcb437f24793ae0899326103bbc885d152c
Saymon Hasan <161878824+Samstatix@users.noreply.github.com>
2026-02-15 17:22:51 +0000

    solve

---
+++ Problem_2618/solve_2618.sql
@@ -0,0 +1,13 @@
+SELECT 
+    p.name, 
+    pr.name, 
+    c.name
+FROM 
+    products p
+INNER JOIN 
+    providers pr ON p.id_providers = pr.id
+INNER JOIN 
+    categories c ON p.id_categories = c.id
+WHERE 
+    pr.name = 'Sansul SA' 
+    AND c.name = 'Imported';
